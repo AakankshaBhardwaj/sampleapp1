e387a3c80027b88e32a3e2c26a82b5ce3c18c9ab
aakanksha <aakanksha@headerlabs.com>
2015-02-21 15:50:22 +0530

    finish log in log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,20 +9,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # Not the final implementation!
-    if @user.save
-flash[:success] = "Welcome to the Sample App!"
-      # Handle a successful save.
-    else
-      render 'new'
-    end
-  end
-
-
-def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
